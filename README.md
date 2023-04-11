5d90b29fcbd1d6a9825ba660c97f4cc81f77c480
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-04-11 22:06:35 +0200

    Large number support using GNU GMP

--- README.md
+++ README.md
@@ -2,8 +2,44 @@
 
 The Game of Googol written in C
 
-Thanks to [Vsauce2](https://www.youtube.com/watch?v=OeJobV4jJG0) and [ChatGPT](ai.com)
+## Credits
 
-To compile program, install GCC and run `gcc -o gog.c`
+- [The Game You Quit - Vsauce2](https://www.youtube.com/watch?v=OeJobV4jJG0)
+- [ChatGPT](ai.com)
+- [3x](https://github.com/Andrej123456789/3x)
+- [GMP tutorial](https://home.cs.colorado.edu/~srirams/courses/csci2824-spr14/gmpTutorial.html)
+- [`Integer-Comparisons` GNU GMP documentation](https://gmplib.org/manual/Integer-Comparisons)
 
-*See Vsauce2's video for biggest chances to win*
+## Installation
+
+### A
+
+- Install GCC
+- Install GNU GMP library
+
+### B
+
+- Run this command: `gcc -lm -lgmp -g .o gog gog.c && ./gog`
+
+## Rules
+
+You have `x` numbers of cards. Each card have a number, between `1` (one) and `10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000` (googol).
+
+Winner is person who finds largest number.
+
+**Be careful, you cannot retroactively choose number few cards before!**
+
+## How to win
+
+<details>
+  <summary><strong>WARNING: If you want to try win this game without any help, (please) don't click me!</strong></summary>
+  
+  Divide number of cards with number `e` (_2.7182818284590452353602874713527..._) and round result to nearest natural number. When your card's position is same as result moment ago, continue playing and when you get bigger number than on card with that position, quit playing.
+
+Example: 10/e ≈ 4
+
+- Card 4: 78 - CONTINUE PLAYING
+- Card 5: 68 - CONTINUE PLAYING
+- Card 6: 81 - QUIT
+
+</details>
--- gog.c
+++ gog.c
@@ -2,58 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <assert.h>
 #include <inttypes.h>
 #include <time.h>
 
+#include "gmp.h"
+
 typedef struct card_T
 {
-    __uint64_t number;
+    mpz_t number;
 } Card;
 
-uint64_t random_uint64(uint64_t x, uint64_t y) 
+void random_mpz(mpz_t result, const mpz_t min, const mpz_t max)
 {
-    if (x >= y) 
-    {
-        fprintf(stderr, "Error: x must be less than y.\n");
-        exit(EXIT_FAILURE);
-    }
+    mpz_t range, random_number;
 
-    uint64_t range = y - x + 1;
-    uint64_t random_number;
+    mpz_init(range);
+    mpz_init(random_number);
 
-    // Generate random number using arc4random()
-    // We need to generate two 32-bit numbers and combine them to form a 64-bit number
-    uint32_t random_number_low = arc4random();
-    uint32_t random_number_high = arc4random();
+    /* Calculate the range */
+    mpz_sub(range, max, min);
 
-    random_number = ((uint64_t)random_number_high << 32) | (uint64_t)random_number_low;
+    /* Generate a random number within the range */
+    unsigned long seed;
 
-    // Scale the random number to the desired range
-    random_number = random_number % range + x;
+    gmp_randstate_t state;
+    gmp_randinit_mt(state);
 
-    return random_number;
+    FILE *urandom = fopen("/dev/urandom", "r");
+    fread(&seed, sizeof(seed), 1, urandom);
+
+    seed = (seed / rand()) / rand();
+    fclose(urandom);
+
+    gmp_randseed_ui(state, seed);
+    mpz_urandomm(random_number, state, range);
+
+    /* Add the minimum value to the random number */
+    mpz_add(result, random_number, min);
+
+    gmp_randclear(state);
+    mpz_clear(random_number);
+    mpz_clear(range);
 }
 
 char* add_underscores(uint64_t number) 
 {
-    // Convert the number to a string
-    char str[21]; // maximum 20 characters for uint64_t plus null terminator
+    /* Convert the number to a string */
+    char str[21]; /* maximum 20 characters for uint64_t plus null terminator */
     sprintf(str, "%llu", number);
 
-    // Count the number of digits in the string
+    /* Number of digits in the string */
     int num_digits = strlen(str);
 
-    // Calculate the number of underscores needed
+    /* Calculate the number of underscores needed */
     int num_underscores = num_digits / 3;
     if (num_digits % 3 == 0) 
     {
         num_underscores--;
     }
 
-    // Allocate memory for the new string
-    char* new_str = malloc(num_digits + num_underscores + 1); // plus null terminator
+    /* Allocate memory for the new string */
+    char* new_str = malloc(num_digits + num_underscores + 1); /* plus null terminator */
 
-    // Copy the digits and underscores into the new string
+    /* Copy the digits and underscores into the new string */
     int j = 0;
     for (int i = 0; i < num_digits; i++) 
     {
@@ -71,37 +83,104 @@ char* add_underscores(uint64_t number)
     return new_str;
 }
 
+char* mpz_separate(mpz_t num)
+{
+    char* num_str = mpz_get_str(NULL, 10, num);
+    int num_len = strlen(num_str);
+    int sep_len = num_len / 3 + ((num_len % 3) == 0 ? -1 : 0);
+    char* sep_str = malloc(num_len + sep_len + 1);
+
+    int i = 0, j = 0;
+    for (; i < num_len; i++, j++) 
+    {
+        sep_str[j] = num_str[i];
+        if (((num_len - i - 1) % 3 == 0) && (i < num_len - 1)) 
+        {
+            sep_str[++j] = '_';
+        }
+    }
+
+    sep_str[j] = '\0';
+
+    free(num_str);
+    return sep_str;
+}
+
+void find_largest_number(mpz_t max_number, Card *cards, size_t num_cards)
+{
+    for (size_t i = 0; i < num_cards; i++) 
+    {
+        if (mpz_cmp(cards[i].number, max_number) > 0) 
+        {
+            mpz_set(max_number, cards[i].number);
+        }
+    }
+}
+
 int main()
 {
-    __uint64_t num_cards = 0;
+    int flag;
+    uint64_t num_cards;
 
     char* input = (char*)malloc(sizeof(char) * 1024);
     char* yes_or_no = (char*)malloc(sizeof(char) * 1024);
 
     printf("Enter number of cards: ");
-    scanf("%s", input);
+    scanf("%1023s", input);
 
     sscanf(input, "%" SCNu64, &num_cards); /* num_cards = strtoull(input, NULL, 0); */
     free(input);
 
-    /* printf("%" SCNu64, num_cards);
-    printf("\n"); */
+    /* ------------------------------------ */
 
     Card cards[num_cards];
 
-    srand(time(NULL));
+    /* ------------------------------------ */
 
-    uint64_t x = random_uint64(1, 18446744073709551615 - 2);
-    uint64_t y = random_uint64(x + 1, 18446744073709551615);
+    mpz_t x, y, one, googol, googol_minus_2, x_plus_1, temporary_number;
 
-    for (__uint64_t i = 0; i < num_cards; i++)
+    mpz_init(x);
+    mpz_init(y);
+
+    mpz_init(one);
+    mpz_init(googol);
+
+    mpz_init(googol_minus_2);
+    mpz_init(x_plus_1);
+    mpz_init(temporary_number);
+
+    /* ------------------------------------ */
+
+    mpz_set_ui(one, 1);
+    mpz_set_ui(googol, 18446744073709551615); /* googol is currently broken */
+
+    /* ------------------------------------ */
+
+    mpz_set_ui(temporary_number, 2);
+    mpz_sub(googol_minus_2, googol, temporary_number);
+    random_mpz(x, one, googol_minus_2);
+
+    mpz_set_ui(temporary_number, 1);
+    mpz_add(x_plus_1, x, temporary_number);
+    random_mpz(y, x_plus_1, googol);
+
+    /* ------------------------------------ */
+    mpz_clear(one);
+    mpz_clear(googol);
+
+    mpz_clear(googol_minus_2);
+    mpz_clear(x_plus_1);
+    mpz_clear(temporary_number);
+
+    for (uint64_t i = 0; i < num_cards; i++)
     {
-        cards[i].number = random_uint64(x, y);
+        mpz_init(cards[i].number);
+        random_mpz(cards[i].number, x, y);
     }
 
-    for (__uint64_t i = 0; i < num_cards; i++)
+    for (uint64_t i = 0; i < num_cards; i++)
     {
-        printf("Number on card %s is: %s\n", add_underscores(i), add_underscores(cards[i].number));
+        printf("Number on card %s is: %s\n", add_underscores(i), mpz_separate(cards[i].number));
         printf("Are you quitting? [yes - 1 | no - 0]\n");
 
         int quit = 0;
@@ -113,56 +192,65 @@ int main()
         {
             printf("\nYou...\n");
 
-            // Find the largest number in the cards array
-            uint64_t max_number = 0;
-            for (int i = 0; i < num_cards; i++) 
+            mpz_t max_number;
+            mpz_init(max_number);
+            
+            for (size_t i = 0; i < num_cards; i++) 
             {
-                if (cards[i].number > max_number) 
+                if (mpz_cmp(cards[i].number, max_number) > 0) 
                 {
-                    max_number = cards[i].number;
+                    mpz_set(max_number, cards[i].number);
                 }
             }
 
-            if (max_number > cards[i].number)
+            if (mpz_cmp(max_number, cards[i].number) > 0) /* +1 */
             {
                 printf("...lost :( \n");
-                printf("Largest number is %s\n", add_underscores(max_number));
+                printf("Largest number is %s\n", mpz_separate(max_number));
             }
 
             else
             {
                 printf("...won :) \n");
-                printf("Largest number is %s\n", add_underscores(max_number));
+                printf("Largest number is %s\n", mpz_separate(max_number));
             }
 
+            mpz_clear(max_number);
             free(yes_or_no);
+            
             exit(0);
         }
     }
 
     printf("\nYou...\n");
 
-    // Find the largest number in the cards array
-    uint64_t max_number = 0;
-    for (int i = 0; i < num_cards; i++) 
+    mpz_t max_number;
+    mpz_init(max_number);
+
+    for (size_t i = 0; i < num_cards; i++) 
     {
-        if (cards[i].number > max_number) 
+        if (mpz_cmp(cards[i].number, max_number) > 0) 
         {
-            max_number = cards[i].number;
+            mpz_set(max_number, cards[i].number);
         }
     }
 
-    if (max_number > cards[num_cards].number)
+    if (mpz_cmp(max_number, cards[num_cards - 1].number) > 0) /* +1 */
     {
         printf("...lost :( \n");
-        printf("Largest number is %s\n", add_underscores(max_number));
+        printf("Largest number is %s\n", mpz_separate(max_number));
     }
 
     else
     {
         printf("...won :) \n");
-        printf("Largest number is %s\n", add_underscores(max_number));
+        printf("Largest number is %s\n", mpz_separate(max_number));
     }
+
+    mpz_clear(max_number);
+
+    mpz_clear(x);
+    mpz_clear(y);
 
     free(yes_or_no);
     return 0;
